9a3e65255b3a18e487badafd9be4359158ccc92d
zach6585 <zmines@wesleyan.edu>
2020-11-22 15:36:46 -0500

    commit tree e89987419720754b7a0ca5029779773801af227c

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
     resources :songs, only: [:index, :show]
   end
   resources :songs
+  namespace :admin do 
+    resources :preferences, only: [:index]
+  end 
 
 end
---
+++ db/migrate/20201122201034_create_preferences.rb
@@ -0,0 +1,9 @@
+class CreatePreferences < ActiveRecord::Migration[5.0]
+  def change
+    create_table :preferences do |t|
+      t.boolean :allow_create_artists
+      t.boolean :allow_create_songs
+      t.timestamps
+    end
+  end
+end
