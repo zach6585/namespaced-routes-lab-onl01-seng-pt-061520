95d25913e909f824ef3ab61729a736c4b3f10eec
zach6585 <zmines@wesleyan.edu>
2020-11-22 15:36:48 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,3 +1,4 @@
+# require 'pry'
 class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
@@ -8,7 +9,12 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    # binding.pry
+    if params.permitted?
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end 
   end
 
   def create
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params.permitted?
+      @song = Song.new
+    else
+      redirect_to songs_path
+    end 
   end
 
   def create
